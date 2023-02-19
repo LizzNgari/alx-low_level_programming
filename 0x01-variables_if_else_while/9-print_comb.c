b0b9f242a296dab5621eb9886c8d375bbbfc9c97
LizzNgari <lyzzngari@gmail.com>
2023-02-19 07:19:27 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,7 +14,8 @@ int main(void)
 	int x;
 
 	for (x = 0 ; x < 10 ; x++)+
-	^{
+        ^ 
+	{
 		putchar((x % 10) + '0')
 		if (x == 9)
 	/* continue to print comma and space except if x == 9*/continue;
