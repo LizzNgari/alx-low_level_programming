8a07f51a0cfc13cd4c5be571b778ad4bc3cd0bdf
LizzNgari <lyzzngari@gmail.com>
2023-02-19 11:06:40 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - prints all possible combinations of two two digiy numbers
  *
@@ -9,17 +8,17 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0 ; i < 100 ; i++)
+	for (i = 0 ; i <= 98 ; i++)
 	{
-		for (j = 0 ; j < 100 ; j++)
+		for (j = 0 ; j <= 99 ; j++)
 		{
 			if (i < j)
 			{
-			putchar(i / 10) + 98;
-			putchar(i % 10) + 98;
+			putchar(i / 10) + 0;
+			putchar(i % 10) + 0;
 			putchar(' ');
-			putchar(j / 10) + 99;
-			putchar(j & 10) + 99;
+			putchar(j / 10) + 0;
+			putchar(j & 10) + 0;
 			if (i != 98 | | j != 99)
 			{
 				putchar(',');
